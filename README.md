8c82df42ffc77333ca1817308eaa08f6c056e68f
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-23 22:14:36 +0100

    Increased module depth, made connection settings a hash

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# rbvami
+# rbvami - a Ruby module for managing vCSA VAMI
 ## Overview
 A Ruby module for configuring vCSA 6.5 VAMI via public REST API
 
--- vSphereREST.rb
+++ vSphereREST.rb
@@ -26,135 +26,137 @@ module VsphereREST
     end
   end
 
-  module VamiAccess
-    # VAMI Access
-    # Get SSH configuration
-    def VamiAccess.get_vami_ssh_config(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/access/ssh')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return body['value']
+  module Vami
+    module Access
+      # VAMI Access
+      # Get SSH configuration
+      def Access.get_vami_ssh_config(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/access/ssh')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return body['value']
+      end
+      # Set SSH configuration
+      def Access.set_vami_ssh_config(hosturi, session, state)
+        # foo
+      end
+      # Get Shell (bash) configuration
+      def Access.get_vami_shell_config(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/access/shell')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return [ body['enabled'], body['timeout'] ]
+      end
+      # Set Shell (bash) configuration
+      def Access.set_vami_shell_config(hosturi, session, state)
+        # foo
+      end
+      # Get Console CLI configuration
+      def Access.get_vami_consolecli_config(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/access/consolecli')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return body['value']
+      end
+      # Set Console CLI configuration
+      def Access.set_vami_consolecli_config(hosturi, session, state)
+        # foo
+      end
+      # Get DCUI configuration
+      def Access.get_vami_dcui_config(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/access/consolecli')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return body['value']
+      end
+      # Set DCUI configuration
+      def Access.set_vami_dcui_config(hosturi, session, state)
+        # foo
+      end
     end
-    # Set SSH configuration
-    def VamiAccess.set_vami_ssh_config(hosturi, session, state)
-      # foo
-    end
-    # Get Shell (bash) configuration
-    def VamiAccess.get_vami_shell_config(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/access/shell')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return [ body['enabled'], body['timeout'] ]
-    end
-    # Set Shell (bash) configuration
-    def VamiAccess.set_vami_shell_config(hosturi, session, state)
-      # foo
-    end
-    # Get Console CLI configuration
-    def VamiAccess.get_vami_consolecli_config(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/access/consolecli')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return body['value']
-    end
-    # Set Console CLI configuration
-    def VamiAccess.set_vami_consolecli_config(hosturi, session, state)
-      # foo
-    end
-    # Get DCUI configuration
-    def VamiAccess.get_vami_dcui_config(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/access/consolecli')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return body['value']
-    end
-    # Set DCUI configuration
-    def VamiAccess.set_vami_dcui_config(hosturi, session, state)
-      # foo
-    end
-  end
 
-  module VamiHealth
-    # VAMI Health Status
-  end
+    module Health
+      # VAMI Health Status
+    end
 
-  module VamiNetworking
-    # VAMI Networking
-    # Get DNS Domains
-    def VamiNetworking.get_dns_domains(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/networking/dns/domains')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return body['value']
-    end
-    # Set DNS Domain list
-    def VamiNetworking.set_dns_domains(hosturi, session, domain_list)
-      # foo
-    end
-    # Add DNS Domain
-    def VamiNetworking.add_dns_domain(hosturi, session, domain)
-      # foo
-    end
-    # Get hostname
-    def VamiNetworking.get_hostname(hosturi, session)
-      url = URI(hosturi+'/rest/appliance/networking/dns/domains')
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(url)
-      request['vmware-api-session-id'] = session
-      response = http.request(request)
-      body = JSON.parse(response.read_body)
-      return body['value']
-    end
-    # Set hostname
-    def VamiNetworking.set_hostname(hosturi, session, hostname)
-      # foo
-    end
-    # Test hostname resolution
-    def VamiNetworking.test_hostname(hosturi, session, hostname)
-      # foo
-    end
-    # Get DNS Servers
-    def VamiNetworking.get_dns_servers(hosturi, session)
-      # foo
-    end
-    # Set DNS Configuration
-    def VamiNetworking.set_dns_config(hosturi, session, mode, server_list)
-      # foo
-    end
-    # Add DNS Server
-    def VamiNetworking.add_dns_server(hosturi, session, server_ip)
-      # foo
-    end
-    # Test DNS Server reachability
-    def VamiNetworking.test_dns_server(hosturi, session, server_ip)
-      # foo
+    module Networking
+      # VAMI Networking
+      # Get DNS Domains
+      def Networking.get_dns_domains(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/networking/dns/domains')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return body['value']
+      end
+      # Set DNS Domain list
+      def Networking.set_dns_domains(hosturi, session, domain_list)
+        # foo
+      end
+      # Add DNS Domain
+      def Networking.add_dns_domain(hosturi, session, domain)
+        # foo
+      end
+      # Get hostname
+      def Networking.get_hostname(hosturi, session)
+        url = URI(hosturi+'/rest/appliance/networking/dns/domains')
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        request = Net::HTTP::Get.new(url)
+        request['vmware-api-session-id'] = session
+        response = http.request(request)
+        body = JSON.parse(response.read_body)
+        return body['value']
+      end
+      # Set hostname
+      def Networking.set_hostname(hosturi, session, hostname)
+        # foo
+      end
+      # Test hostname resolution
+      def Networking.test_hostname(hosturi, session, hostname)
+        # foo
+      end
+      # Get DNS Servers
+      def Networking.get_dns_servers(hosturi, session)
+        # foo
+      end
+      # Set DNS Configuration
+      def Networking.set_dns_config(hosturi, session, mode, server_list)
+        # foo
+      end
+      # Add DNS Server
+      def Networking.add_dns_server(hosturi, session, server_ip)
+        # foo
+      end
+      # Test DNS Server reachability
+      def Networking.test_dns_server(hosturi, session, server_ip)
+        # foo
+      end
     end
   end
 end
