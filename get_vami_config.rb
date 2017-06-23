60fcf17455c0d0f3cc9d276266e705a434e1e7c7
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-23 20:09:52 +0100

    Created submodules, added additional VAMI access functions

--- get_vami_config.rb
+++ get_vami_config.rb
@@ -6,12 +6,18 @@ hosturi = 'https://192.168.1.222'
 username = 'administrator@vsphere.local'
 password = 'VMware1!'
 
-session = VSphereREST.get_session(hosturi, username, password)
+session = VSphereREST::Session.get_session(hosturi, username, password)
 puts 'Session: ' + session
 
-ssh_config = VSphereREST.get_vami_ssh_config(hosturi,session)
+ssh_config = VSphereREST::Vami.get_vami_ssh_config(hosturi,session)
 puts 'SSH access: ' + (ssh_config ? 'enabled' : 'disabled')
 
-bash_config = VSphereREST.get_vami_bash_config(hosturi,session)
+bash_config = VSphereREST::Vami.get_vami_shell_config(hosturi,session)
 puts 'BASH access: ' + (bash_config[0] ? 'enabled' : 'disabled') + \
      ' (timeout: ' + (bash_config[1] ? bash_config[1] : '0') + ' minutes)'
+
+dcui_config = VSphereREST::Vami.get_vami_dcui_config(hosturi,session)
+puts 'DCUI config: ' + (dcui_config ? 'enabled' : 'disabled')
+
+consolecli_config = VSphereREST::Vami.get_vami_consolecli_config(hosturi,session)
+puts 'Console CLI config: ' + (consolecli_config ? 'enabled' : 'disabled')
--- vSphereREST.rb
+++ vSphereREST.rb
@@ -1,44 +1,76 @@
 module VSphereREST
+
   # Import dependencies
   require 'uri'
   require 'net/http'
   require 'openssl'
   require 'json'
 
-  def VSphereREST.get_session(hosturi, username, password)
-    url = URI(hosturi+'/rest/com/vmware/cis/session')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new(url)
-    request.basic_auth(username, password)
-    request['cookie'] = 'vmware-api-session-id'
-    response = http.request(request)
-    body = JSON.parse(response.read_body)
-    return body['value']
+  module Session
+    # Session
+    def Session.get_session(hosturi, username, password)
+      url = URI(hosturi+'/rest/com/vmware/cis/session')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Post.new(url)
+      request.basic_auth(username, password)
+      request['cookie'] = 'vmware-api-session-id'
+      response = http.request(request)
+      body = JSON.parse(response.read_body)
+      return body['value']
+    end
   end
 
-  def VSphereREST.get_vami_ssh_config(hosturi, session)
-    url = URI(hosturi+'/rest/appliance/access/ssh')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(url)
-    request['vmware-api-session-id'] = session
-    response = http.request(request)
-    body = JSON.parse(response.read_body)
-    return body['value']
-  end
-
-  def VSphereREST.get_vami_bash_config(hosturi, session)
-    url = URI(hosturi+'/rest/appliance/access/shell')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(url)
-    request['vmware-api-session-id'] = session
-    response = http.request(request)
-    body = JSON.parse(response.read_body)
-    return [ body['enabled'], body['timeout'] ]
+  module Vami
+    # VAMI Access
+    # Get SSH configuration
+    def Vami.get_vami_ssh_config(hosturi, session)
+      url = URI(hosturi+'/rest/appliance/access/ssh')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(url)
+      request['vmware-api-session-id'] = session
+      response = http.request(request)
+      body = JSON.parse(response.read_body)
+      return body['value']
+    end
+    # Set SSH configuration
+    def Vami.set_vami_ssh_config(hosturi, session, state)
+    end
+    def Vami.get_vami_shell_config(hosturi, session)
+      url = URI(hosturi+'/rest/appliance/access/shell')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(url)
+      request['vmware-api-session-id'] = session
+      response = http.request(request)
+      body = JSON.parse(response.read_body)
+      return [ body['enabled'], body['timeout'] ]
+    end
+    def Vami.get_vami_consolecli_config(hosturi, session)
+      url = URI(hosturi+'/rest/appliance/access/consolecli')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(url)
+      request['vmware-api-session-id'] = session
+      response = http.request(request)
+      body = JSON.parse(response.read_body)
+      return body['value']
+    end
+    def Vami.get_vami_dcui_config(hosturi, session)
+      url = URI(hosturi+'/rest/appliance/access/consolecli')
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(url)
+      request['vmware-api-session-id'] = session
+      response = http.request(request)
+      body = JSON.parse(response.read_body)
+      return body['value']
+    end
   end
 end
