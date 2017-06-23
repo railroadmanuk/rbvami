0f0d6f2de40b73a6cb5e27b534339e24c844172d
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-23 19:37:54 +0100

    Added get_vami_ssh_config function

--- get_vami_config.rb
+++ get_vami_config.rb
@@ -3,11 +3,11 @@ $LOAD_PATH << '.'
 require 'VSphereREST'
 
 hosturi = 'https://192.168.1.222'
-username = 'root'
+username = 'administrator@vsphere.local'
 password = 'VMware1!'
 
 session = VSphereREST.get_session(hosturi, username, password)
 puts 'Session: ' + session
 
 ssh_config = VSphereREST.get_vami_ssh_config(hosturi,session)
-puts 'SSH Config: ' + ssh_config
+puts 'SSH access: ' + (ssh_config ? 'enabled' : 'disabled')
--- vSphereREST.rb
+++ vSphereREST.rb
@@ -6,19 +6,27 @@ module VSphereREST
   require 'json'
 
   def VSphereREST.get_session(hosturi, username, password)
-    url = URI(hosturi+"/rest/com/vmware/cis/session")
+    url = URI(hosturi+'/rest/com/vmware/cis/session')
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Post.new(url)
     request.basic_auth(username, password)
-    request["cookie"] = 'vmware-api-session-id'
+    request['cookie'] = 'vmware-api-session-id'
     response = http.request(request)
-    token = JSON.parse(response.read_body)
-    return token['value']
+    body = JSON.parse(response.read_body)
+    return body['value']
   end
 
   def VSphereREST.get_vami_ssh_config(hosturi, session)
-    return 'ENABLED'
+    url = URI(hosturi+'/rest/appliance/access/ssh')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request['vmware-api-session-id'] = session
+    response = http.request(request)
+    body = JSON.parse(response.read_body)
+    return body['value']
   end
 end
