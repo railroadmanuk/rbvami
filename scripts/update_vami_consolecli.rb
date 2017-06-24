46e3c0c7e52720b12a513ee197b39eca074f92d8
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-24 18:17:37 +0100

    Tidied directory structure

---
+++ scripts/update_vami_consolecli.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+$LOAD_PATH << '.'
+# Load custom vSphereREST module
+require 'vSphereREST'
+
+# Environment specific details
+connection = {
+  'hosturi' => 'https://192.168.1.222',
+  'username' => 'administrator@vsphere.local',
+  'password' => 'VMware1!'
+}
+
+# Get our session token and print it out
+session = VsphereREST::Session.get_session(connection['hosturi'], \
+          connection['username'], connection['password'])
+puts 'Session: ' + session
+
+print "========================\nUpdate Console CLI access\n========================\n"
+print "Type enabled or disabled:\n"
+input = gets.strip
+
+config_consolecli = VsphereREST::Vami::Access.set_vami_consolecli_config(connection['hosturi'],session,input)
+if config_consolecli == '200'
+  print "SUCCESS: Update was successful, console CLI is now " + input + "\n"
+else
+  print config_consolecli
+end
